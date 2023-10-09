35a702c27e42dad9166a698273d8f0dd65d9cbee
38Dann <38dunn@gmail.com>
2023-10-09 13:30:33 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -2,6 +2,5 @@ import setuptools
 
 setuptools.setup(
     name="phis_seg_doc",
-    package_dir={"doc_img_utils": "", "phis_seg_doc": ""},
-    packages=["doc_img_utils", "phis_seg_doc"]
+    packages=setuptools.find_packages()
 )

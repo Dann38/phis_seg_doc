766ca1c12f7170bff1967770f3bda49e5e8518c5
Даниил Копылов <it-daniil@yandex.ru>
2023-11-02 22:48:54 +0800

    set coef and method

--- upload_files/api.py
+++ upload_files/api.py
@@ -21,12 +21,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/upload", methods=["POST"])
+@app.route("/upload_image", methods=["POST"])
 def upload():
+    # bytes_file = request.files["file"].stream.read()
     requests_upload = requests.post(f'{host_classifier}/file/upload-file', files=request.files)
     id_image = json.loads(requests_upload.content)["id_image"]
-    requests.post(f'{host_classifier}/file/classify/{id_image}')
+
     return {"id_image": id_image}
+
+
+@app.route("/classify_image/<int:id_image>", methods=["POST"])
+def classifier(id_image):
+    method = int(request.form["method_classifier"])
+    coef = float(request.form["coef"])
+    requests.post(f'{host_classifier}/file/classify/{id_image}', json={"method": method, "coef": coef})
+    return {}
 
 
 @app.route("/get_image_result/<int:id_image>", methods=["GET"])
@@ -42,4 +51,10 @@ def get_image_origin(id_image):
 @app.route("/get_history", methods=["GET"])
 def get_history():
     content = requests.get(f'{host_classifier}/file/get_history').content
+    return json.loads(content)
+
+
+@app.route("/get_set_classifier/<int:id_image>", methods=["GET"])
+def get_set_classifier(id_image):
+    content = requests.get(f'{host_classifier}/file/get_set_classifier/{id_image}').content
     return json.loads(content)
--- upload_files/static/js/upload.js
+++ upload_files/static/js/upload.js
@@ -14,7 +14,22 @@ var addHistoryImg = function(id, pr=false) {
     $(".button_id_send").on("click", btnClassClick);
  }
 
- var addHistory = function() {
+var addSetClassify = function(id_image) {
+    const xhr_set_classify = new XMLHttpRequest();
+    xhr_set_classify.open("GET", "/get_set_classifier/"+id_image);
+    xhr_set_classify.send();
+    xhr_set_classify.onload = function() {
+        if (xhr_set_classify.status == 200) {
+            var response_set_classify = $.parseJSON(xhr_set_classify.response);
+            $("#method-input")[0].value = response_set_classify.method;
+            $("#coef-input")[0].value = response_set_classify.coef;
+            $("#coef-input-text")[0].innerHTML =  response_set_classify.coef;
+        }
+    }
+    $(".button_id_send").on("click", btnClassClick);
+ }
+
+var addHistory = function() {
     const xhr_history = new XMLHttpRequest();
     xhr_history.open("GET", "/get_history");
     xhr_history.send();
@@ -29,19 +44,26 @@ var addHistoryImg = function(id, pr=false) {
     }
     $(".button_id_send").on("click", btnClassClick);
  }
-addHistory();
 
-$("#button_send").click(function(){
+var btnClassClick = function(e){
+    d = new Date();
+    $(".upload-file").attr("src", "/get_image_origin/"+e.target.dataset.rowId+"?"+d.getTime());
+    $(".result-file").attr("src", "/get_image_result/"+e.target.dataset.rowId+"?"+d.getTime());
+    $("#work-id").attr("data-row-id", e.target.dataset.rowId);
+    addSetClassify(e.target.dataset.rowId)
+}
+
+$("#button_upload").click(function(){
     const fileInput = document.getElementById('file-input'); 
     
-    const file = fileInput.files[0]; 
+    const file = fileInput.files[0];
 
     const xhr = new XMLHttpRequest(); 
-    const formData = new FormData(); 
+    const formData = new FormData();
 
-    formData.append('file', file); 
+    formData.append('file', file);
 
-    xhr.open('POST', '/upload');
+    xhr.open('POST', '/upload_image');
     xhr.send(formData);
 
     xhr.onload = function() {
@@ -51,13 +73,39 @@ $("#button_send").click(function(){
             var response_upload = $.parseJSON(xhr.response);
             addHistoryImg(response_upload.id_image, pr=true);
             $(".upload-file").attr("src", "/get_image_origin/"+response_upload.id_image);
-            $(".result-file").attr("src", "/get_image_result/"+response_upload.id_image);
+            $(".result-file").attr("src", "/get_image_origin/"+response_upload.id_image);
+            $("#work-id").attr("data-row-id", response_upload.id_image)
         }
       };
 });
 
-var btnClassClick = function(e){
-    $(".upload-file").attr("src", "/get_image_origin/"+e.target.dataset.rowId);
-    $(".result-file").attr("src", "/get_image_result/"+e.target.dataset.rowId);
-}
 
+$("#button_classifier").click(function(){
+    const id_image = $("#work-id")[0].dataset.rowId;
+
+    const method_classifier = document.getElementById('method-input').value;
+    const coef = document.getElementById('coef-input').value;
+    console.log(id_image, method_classifier, coef)
+
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+
+
+    formData.append('method_classifier', method_classifier);
+    formData.append('coef', coef);
+
+    xhr.open('POST', '/classify_image/'+id_image);
+    xhr.send(formData);
+
+    xhr.onload = function() {
+        if (xhr.status != 200) {
+          alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            d = new Date();
+            $(".result-file").attr("src", "/get_image_result/"+id_image+"?"+d.getTime());
+            addSetClassify(id_image);
+        }
+      };
+});
+
+addHistory();
